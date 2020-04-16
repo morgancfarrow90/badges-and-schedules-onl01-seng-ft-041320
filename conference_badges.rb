529363a4bdedd987a3dc637d1def7b54d5d748cb
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 01:39:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,8 @@
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
    return "Hello, my name is #{name}."
 end
-
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(array)
   speakarray =[]
