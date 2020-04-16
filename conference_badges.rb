7ecaf63e4ea19e1ae883aef3daf27d0468b6e939
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 01:37:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,13 +4,15 @@ end
 
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(speakers)
+def batch_badge_creator(array)
   speakarray =[]
-  speakers.each do |name|
+  speakarray.each do |name|
     speakarray.push("Hello, my name is #{name}.")
   end
   return speakarray
 end 
+
+batch_badge_creator(speakers)
   
 def assign_rooms(array)
   speakarray = []
